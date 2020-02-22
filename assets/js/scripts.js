ffa61077471a1cfe6ca562b2dbeefb07955a5255
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-02-22 20:15:58 -0300

    resolvendo problema na estilização dos links ao ativar o scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,21 @@
 var menu = document.querySelector('header .menu');
 var navMenu = document.querySelector('header nav');
-var listMenu = document.querySelectorAll('li a');
+var listMenu = document.querySelectorAll('nav a');
 
 window.onscroll = function(){
     var top = window.pageYOffset || document.documentElement.scrollTop;
     if( top > 400 ) {
         menu.classList.add('menuScroll', 'transition','w-100', 'shadow');
         navMenu.classList.add('w-100');
+        for(links of listMenu){
+            links.classList.add('scrollColor');
+        }        
 
     } else {
         menu.classList.remove('menuScroll', 'scrollColor', 'transition','w-100', 'shadow');
         navMenu.classList.remove('w-100');
+        for(links of listMenu){
+            links.classList.remove('scrollColor');
+        } 
     }
 }
